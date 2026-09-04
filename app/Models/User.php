5b0a667d82be50e987afb1e215a6a3ac6f68923c
rossyamara-blip <rossyamara76@gmail.com>
2026-09-04 20:26:27 +0700

    secure

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,8 +29,7 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
  */
-#[Fillable(['name', 'username', // Pastikan baris ini sudah ditambahkan, jika belum tambahkan
- 'email', 'password', 'role'])]
+#[Fillable(['name', 'username', 'email', 'password', 'role'])]
 #[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
 class User extends Authenticatable implements PasskeyUser
 {
